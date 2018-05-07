1a932e54ea31145f926792be061b546b0c8a5b79
90sirengo <35330015+90sirengo@users.noreply.github.com>
2018-05-07 14:48:39 +0300

    Add files via upload

---
+++ gataya.php
@@ -0,0 +1,21 @@
+<?php 
+//index page php//
+
+if (isset($_POST['submit'])) {
+	echo "<b><strong>Thank you for your message.We will get back to you promptly</b></strong>";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ ?>
